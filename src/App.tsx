993f9a7faad2a453488dd2f79020de18ef8d0528
WilfredoN <wilfredochannel@gmail.com>
2024-04-05 00:37:12 +0300

    base markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1 className="text-8xl">Portfolio</h1>
+      </header>
+      <main>
+        <section className="text-left">
+          <h2 className="text-4xl">About Me</h2>
+          <p>
+            Hi, I'm a Software Developer. I love to build web applications from
+            start to finish.
+            <br /> I have experience in Java, Spring on the backend, and React
+            on the frontend.
+          </p>
+        </section>
+        <section className="border max-w-96 rounded-xl">
+          <h2 className="border-b text-4xl">Skills</h2>
+          <ul className="text-3xl">
+            <li className="flex items-center justify-center">
+              React
+              <img
+                className="ml-2 w-8"
+                src="src\assets\react-original.svg"
+              />{' '}
+            </li>
+            <li className="flex items-center justify-center">
+              TypeScript
+              <img
+                className="ml-2 w-8"
+                src="src\assets\typescript-original.svg"
+              />
+            </li>
+            <li className="flex items-center justify-center">
+              Vite
+              <img className="ml-2 w-8" src="src\assets\vitejs-original.svg" />
+            </li>
+            <li className="flex items-center justify-center">
+              TailwindCSS
+              <img
+                className="ml-2 w-8"
+                src="src\assets\tailwindcss-original.svg"
+              />
+            </li>
+            <li className="flex items-center justify-center">
+              Java
+              <img className="ml-2 w-8" src="src\assets\java-original.svg" />
+            </li>
+            <li className="flex items-center justify-center">
+              Spring
+              <img className="ml-2 w-8" src="src\assets\spring-original.svg" />
+            </li>
+            <li className="flex items-center justify-center">
+              Docker
+              <img className="ml-2 w-8" src="src\assets\docker-original.svg" />
+            </li>
+          </ul>
+        </section>
+        <section>{/* TODO: Logo's */}</section>
+      </main>
+      <footer>
+        <p>My portfolio built with Vite + React + TypeScript + TailwindCSS</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
