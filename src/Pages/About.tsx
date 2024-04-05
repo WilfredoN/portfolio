0453ec452f69a14c5b78a030acffe6021f72a01a
WilfredoN <wilfredochannel@gmail.com>
2024-04-05 17:15:23 +0300

    Update image paths in About.tsx

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -13,21 +13,21 @@ const About = () => {
           <ul className="text-3xl">
             <li className="flex items-center justify-center">
               Java
-              <img className="ml-2 w-8" src="src\assets\java-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/java-original.svg" />
             </li>
             <li className="flex items-center justify-center">
               HTML
-              <img className="ml-2 w-8" src="src\assets\html5-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/html5-original.svg" />
             </li>
             <li className="flex items-center justify-center">
               CSS
-              <img className="ml-2 w-8" src="src\assets\css3-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/css3-original.svg" />
             </li>
             <li className="flex items-center justify-center">
               TypeScript
               <img
                 className="ml-2 w-8"
-                src="src\assets\typescript-original.svg"
+                src="src/assets/typescript-original.svg"
               />
             </li>
           </ul>
@@ -37,27 +37,27 @@ const About = () => {
           <ul className="text-3xl">
             <li className="flex items-center justify-center">
               React
-              <img className="ml-2 w-8" src="src\assets\react-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/react-original.svg" />
             </li>
 
             <li className="flex items-center justify-center">
               Vite
-              <img className="ml-2 w-8" src="src\assets\vitejs-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/vitejs-original.svg" />
             </li>
             <li className="flex items-center justify-center">
               TailwindCSS
               <img
                 className="ml-2 w-8"
-                src="src\assets\tailwindcss-original.svg"
+                src="src/assets/tailwindcss-original.svg"
               />
             </li>
             <li className="flex items-center justify-center">
               Spring
-              <img className="ml-2 w-8" src="src\assets\spring-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/spring-original.svg" />
             </li>
             <li className="flex items-center justify-center">
               Docker
-              <img className="ml-2 w-8" src="src\assets\docker-original.svg" />
+              <img className="ml-2 w-8" src="src/assets/docker-original.svg" />
             </li>
           </ul>
         </section>
