28832184017e6822e2f6b63691c295bd0f43a7cc
WilfredoN <wilfredochannel@gmail.com>
2024-04-21 13:58:38 +0300

    disable animations for mobile to deacrease unexpected behavior of header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -22,26 +22,32 @@ const Header = ({
     return () => window.removeEventListener('scroll', checkScrollTop);
   }, []);
 
-  const totalScrollHeight =
-    document.documentElement.scrollHeight - window.innerHeight;
-  const scaleFactor =
-    Math.max(0.85, 1 - scrollPosition / totalScrollHeight / 2) || 0.85;
+  const isMobile = window.innerWidth <= 640;
+
+  const totalScrollHeight = isMobile
+    ? 0
+    : document.documentElement.scrollHeight - window.innerHeight;
+  const scaleFactor = isMobile
+    ? 1
+    : Math.max(0.85, 1 - scrollPosition / totalScrollHeight / 2) || 0.85;
   const handlePageChange = (page: 'about' | 'projects') => {
     onPageChange(page);
-    setScrollPosition(0);
+    if (!isMobile) {
+      setScrollPosition(0);
+    }
   };
 
   return (
     <motion.header
       className={`${theme} flex flex-col h-fit w-full md:w-fit items-center mb-8 rounded-3xl md:rounded-full ${
-        scrollPosition > 0
+        !isMobile && scrollPosition > 0
           ? 'sticky top-5 z-10 px-8 py-4 transition-all duration-300 '
           : 'mt-5 px-12 py-6 transition-all duration-175 '
       }`}
       style={{
-        transform: `scale(${scaleFactor})`,
+        transform: isMobile ? 'none' : `scale(${scaleFactor})`,
       }}
-      whileHover={{ transform: `scale(${scaleFactor * 1.1})` }}
+      whileHover={isMobile ? {} : { transform: `scale(${scaleFactor * 1.1})` }}
     >
       <nav className="w-full flex md:flex-row flex-col justify-center">
         <CustomNavLink
