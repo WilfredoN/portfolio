aa6ffaa28c4b9521531b1c6f1c5c6014c7eeca63
Nikita Afanasiev <nikita.afanasyevnn@gmail.com>
2024-08-24 14:13:57 +0300

    fix: winwin link fix

--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -56,7 +56,7 @@ export const ProjectDescription = () => {
 					alignItems: 'center'
 				}}
 			>
-				<a href="https://www.winwin.travel/demo-request">
+				<a href="https://www.winwin.travel/">
 					<motion.img
 						src="assets/winwin.png"
 						className={`${theme === 'dark' ? 'bg-gray-200' : ''} px-4 py-2 rounded-md my-2`}
@@ -68,7 +68,7 @@ export const ProjectDescription = () => {
 					best hotel room for their needs. <br />
 					If you wanna get demo - contact{' '}
 					<a
-						href="https://www.winwin.travel/demo-request"
+						href="https://www.winwin.travel/"
 						className="text-blue-400"
 					>
 						here
