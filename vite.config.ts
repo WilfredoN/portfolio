c170dae0e884f8c06083019167eca2643e6bd925
WilfredoN <thekillermaps@gmail.com>
2025-03-26 11:08:56 +0200

    refactor: security headers, packages update

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,23 @@ export default defineConfig({
 				{
 					src: 'public/*',
 					dest: 'assets/'
+				},
+				{
+					src: 'public/_headers',
+					dest: './'
 				}
 			]
 		})
-	]
+	],
+	server: {
+		headers: {
+			'Content-Security-Policy':
+				"default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://i.ibb.co https://raw.githubusercontent.com https://capybara.cx.ua; connect-src 'self' https://www.google-analytics.com https://raw.githubusercontent.com; frame-ancestors 'self'; object-src 'none'; base-uri 'self'",
+			'X-Frame-Options': 'SAMEORIGIN',
+			'X-Content-Type-Options': 'nosniff',
+			'Referrer-Policy': 'strict-origin-when-cross-origin',
+			'Permissions-Policy':
+				'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+		}
+	}
 })
