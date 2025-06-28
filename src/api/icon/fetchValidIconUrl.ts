9888acb246ba912a2852ad0e9d494edd5c73d428
WilfredoN <thekillermaps@gmail.com>
2025-06-28 15:59:19 +0300

    refactor: reorganize fetchValidIconUrl function and remove unused useCapybaraImageAnimation component

--- src/api/icon/fetchValidIconUrl.ts
+++ src/api/icon/fetchValidIconUrl.ts
@@ -1,24 +1,38 @@
 import { getIconUrl } from '../../utils/getIconUrl'
 
-export async function fetchValidIconUrl(
+const iconUrlCache = new Map<string, Promise<string | undefined>>()
+
+export const fetchValidIconUrl = async (
 	iconName: string
-): Promise<string | undefined> {
-	const originalUrl = getIconUrl(iconName, 'original')
-	const plainUrl = getIconUrl(iconName, 'plain')
-
-	try {
-		const res = await fetch(originalUrl, { method: 'HEAD' })
-		if (res.ok) return originalUrl
-	} catch (error) {
-		console.log(`Error fetching original icon: ${error}`)
+): Promise<string | undefined> => {
+	if (iconUrlCache.has(iconName)) {
+		return iconUrlCache.get(iconName)!
 	}
 
-	try {
-		const res = await fetch(plainUrl, { method: 'HEAD' })
-		if (res.ok) return plainUrl
-	} catch (error) {
-		console.log(`Error fetching plain icon: ${error}`)
+	const fetchIconUrl = async (
+		iconName: string
+	): Promise<string | undefined> => {
+		const originalUrl = getIconUrl(iconName, 'original')
+		const plainUrl = getIconUrl(iconName, 'plain')
+
+		try {
+			const res = await fetch(originalUrl, { method: 'HEAD' })
+			if (res.ok) return originalUrl
+		} catch (error) {
+			console.log(`Error fetching original icon: ${error}`)
+		}
+
+		try {
+			const res = await fetch(plainUrl, { method: 'HEAD' })
+			if (res.ok) return plainUrl
+		} catch (error) {
+			console.error(`Error fetching plain icon: ${error}`)
+		}
+
+		return undefined
 	}
 
-	return undefined
+	const fetchPromise = (async () => await fetchIconUrl(iconName))()
+	iconUrlCache.set(iconName, fetchPromise)
+	return fetchPromise
 }
