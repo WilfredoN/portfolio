4c359fadccc2f2a0b5d14c9c7cd56e4fdd991be5
WilfredoN <thekillermaps@gmail.com>
2025-06-28 15:51:25 +0300

    refactor: remove DownloadButton from CapybaraImage and simplify MainInfo component

--- src/components/about/MainInfo.tsx
+++ src/components/about/MainInfo.tsx
@@ -1,16 +1,11 @@
-import { CapybaraImage } from '../image/CapybaraImage'
-import { DownloadButton } from '../input/DownloadButton'
-import { MainInfoText } from './MainInfoText'
 import React from 'react'
+import { CapybaraImage } from '../image/CapybaraImage'
+import { MainInfoText } from './MainInfoText'
 
 export const MainInfo = React.memo(() => {
 	return (
 		<div className="flex m-0 flex-col items-center justify-center max-w-screen-2xl relative">
-			<CapybaraImage>
-				<div className="absolute bottom-4 right-4 z-10">
-					<DownloadButton pdfUrl="assets/resume.pdf" />
-				</div>
-			</CapybaraImage>
+			<CapybaraImage />
 			<MainInfoText />
 		</div>
 	)
--- src/components/image/CapybaraImage.tsx
+++ src/components/image/CapybaraImage.tsx
@@ -1,29 +1,19 @@
 import React from 'react'
-import { useCapybaraImageAnimation } from './useCapybaraImageAnimation'
+import { DownloadButton } from '../input/DownloadButton'
 
 interface CapybaraImageProps {
 	children?: React.ReactNode
 }
 
 export const CapybaraImage = React.memo(({ children }: CapybaraImageProps) => {
-	const { imgRef, handleMouseMove } = useCapybaraImageAnimation()
-
 	return (
 		<div className="flex items-center justify-center max-w-[35rem] relative">
-			<div
-				className="flex items-center justify-center overflow-hidden border-2 xl:w-full sm:w-1/2 rounded-full relative"
-				onMouseMove={handleMouseMove}
-				style={{ perspective: '1000px' }}
-			>
+			<DownloadButton pdfUrl="assets/resume.pdf" />
+			<div className="flex items-center justify-center overflow-hidden border-2 xl:w-full sm:w-1/2 rounded-full relative">
 				<img
-					ref={imgRef}
 					src="assets/capybara_binary.png"
 					alt="Capybara image in zeros and ones style"
-					className="scale-105 rounded-full transition-transform duration-300 ease-out"
-					style={{
-						transition: 'transform 0.3s ease-out, opacity 1s ease-in',
-						opacity: '0'
-					}}
+					className="scale-105 rounded-full"
 					draggable="false"
 				/>
 				{children}
