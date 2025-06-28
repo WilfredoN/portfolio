9d283a1b0388577d1d07beeeda7f204b48e3a977
WilfredoN <thekillermaps@gmail.com>
2025-06-28 16:00:59 +0300

    refactor: simplify ProjectDescription component by removing unnecessary fragment

--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -6,12 +6,10 @@ export const ProjectDescription = () => {
 	return (
 		<motion.section className="flex flex-row gap-8 flex-wrap items-center justify-center p-4 w-full">
 			{projects.map(project => (
-				<>
-					<ProjectCard
-						key={project.title}
-						{...project}
-					/>
-				</>
+				<ProjectCard
+					key={project.title}
+					{...project}
+				/>
 			))}
 			<motion.h1 className="text-4xl font-bold text-center">
 				More projects coming soon...
