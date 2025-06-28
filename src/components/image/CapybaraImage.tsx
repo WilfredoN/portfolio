38ca16ac3d7163da5518760750af94c2c8135667
WilfredoN <thekillermaps@gmail.com>
2025-06-28 16:08:22 +0300

    refactor: add select-none class to CapybaraImage container for improved text selection handling

--- src/components/image/CapybaraImage.tsx
+++ src/components/image/CapybaraImage.tsx
@@ -9,7 +9,7 @@ export const CapybaraImage = React.memo(({ children }: CapybaraImageProps) => {
 	return (
 		<div className="flex items-center justify-center max-w-[35rem] relative">
 			<DownloadButton pdfUrl="assets/resume.pdf" />
-			<div className="flex items-center justify-center overflow-hidden border-2 xl:w-full sm:w-1/2 rounded-full relative">
+			<div className="flex items-center justify-center select-none overflow-hidden border-2 xl:w-full sm:w-1/2 rounded-full relative">
 				<img
 					src="assets/capybara_binary.png"
 					alt="Capybara image in zeros and ones style"
