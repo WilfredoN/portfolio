5523f7bb11cf63eb174078bce9595d906f962b1d
WilfredoN <thekillermaps@gmail.com>
2025-07-02 02:01:02 +0300

    dev: sorting import linter enchancement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import antfu from '@antfu/eslint-config'
+import react from 'eslint-plugin-react'
 
 export default antfu({
   formatters: false,
   stylistic: false,
-
+  plugins: {
+    react
+  },
   rules: {
     'antfu/no-top-level-await': 'off',
     'curly': ['error', 'all'],
@@ -13,6 +16,13 @@ export default antfu({
     'perfectionist/sort-imports': ['error', {
       internalPattern: ['^@/'],
     }],
+    'perfectionist/sort-jsx-props': ['error', {
+      type: 'alphabetical',
+      order: 'asc',
+      groups: ['shorthand', 'multiline', 'unknown']
+    }],
+    'perfectionist/sort-object-types': ['error'],
+    'perfectionist/sort-interfaces': ['error'],
     'antfu/top-level-function': 'off',
     'prefer-arrow-callback': ['error'],
     'unused-imports/no-unused-imports': 'error',
@@ -20,5 +30,23 @@ export default antfu({
       'warn',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
+    'react/jsx-sort-props': ['error', {
+      callbacksLast: true,
+      shorthandFirst: true,
+      noSortAlphabetically: false,
+      reservedFirst: true
+    }],
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/member-ordering': ['error', {
+      default: {
+        memberTypes: [
+          'signature',
+          'field',
+          'constructor',
+          'method'
+        ],
+        order: 'alphabetically'
+      }
+    }]
   },
 })
