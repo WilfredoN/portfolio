aa8128c80ad5271400a4be4be29945cd29b39cb2
WilfredoN <thekillermaps@gmail.com>
2025-07-02 01:54:55 +0300

    dev: sync eslint + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  formatters: false,
+  stylistic: false,
+
   rules: {
     'antfu/no-top-level-await': 'off',
-    'style/no-tabs': 'off',
-    'style/jsx-quotes': 'off',
-    'style/comma-dangle': 'off',
     'curly': ['error', 'all'],
-    'style/padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: ['const', 'let', 'var', 'if', 'for', 'while', 'switch', 'block', 'block-like'], next: 'if' },
-    ],
     'node/no-process-env': 'off',
     'node/prefer-global/process': 'off',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
@@ -20,12 +15,10 @@ export default antfu({
     }],
     'antfu/top-level-function': 'off',
     'prefer-arrow-callback': ['error'],
-    'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
-
   },
 })
