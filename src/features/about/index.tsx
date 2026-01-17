ffd2a0c01f2dc800b1892c2c877f6cc4e9357362
WilfredoN <wilfredochannel@gmail.com>
2026-01-17 16:20:20 +0200

    fix: tries to remove initial layout shift

--- src/features/about/index.tsx
+++ src/features/about/index.tsx
@@ -10,15 +10,17 @@ export const About = () => {
   // const isMobile = useIsMobile()
 
   return (
-    <article className='z-[5] mt-8 flex max-w-screen-2xl flex-col items-center justify-center bg-[var(--color-bg)] text-[var(--color-text)]'>
+    <article className='z-5 mt-8 flex max-w-screen-2xl flex-col items-center justify-center bg-(--color-bg) text-(--color-text)'>
       <div className='relative m-0 flex max-w-screen-2xl flex-col items-center justify-center'>
-        <div className='relative flex max-w-[35rem] items-center justify-center'>
+        <div className='relative flex max-w-140 items-center justify-center'>
           <Resume />
           <div
             className={clsx(
               'relative flex items-center justify-center overflow-hidden rounded-full border-2 select-none',
-              'xl:min-h-[560px] xl:w-full xl:min-w-[560px]'
+              'xl:min-h-[560px] xl:w-full xl:min-w-[560px]',
+              'transition-none'
             )}
+            style={{ minWidth: 560, minHeight: 560, width: '100%' }}
           >
             <Capybara url='capybara_binary_compressed_v2.avif' />
           </div>
