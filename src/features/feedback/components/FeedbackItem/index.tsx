a9bd5e8c100f9720ac008c5ac853d12b57dcb2d1
WilfredoN <wilfredochannel@gmail.com>
2026-07-31 20:30:52 +0300

    fix: remove quotes from feedback item

--- src/features/feedback/components/FeedbackItem/index.tsx
+++ src/features/feedback/components/FeedbackItem/index.tsx
@@ -41,7 +41,7 @@ export const FeedbackItem = ({ feedback }: FeedbackItemProps) => (
         >
           <path d='M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h4v10h-10z' />
         </svg>
-        <p className='pl-6 text-lg leading-relaxed italic'>"{feedback.text}"</p>
+        <p className='pl-6 text-lg leading-relaxed italic'>{feedback.text}</p>
       </div>
     </div>
 
